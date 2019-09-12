9ccfe699d29d7568e3110bbe7451297fee06d339
aaron blum <blumaa@gmail.com>
2019-09-12 09:40:24 -0500

    added view activities by all feature

--- lib/baby.rb
+++ lib/baby.rb
@@ -3,4 +3,8 @@ class Baby < ActiveRecord::Base
     has_many :baby_users
     has_many :users, through: :baby_users
 
+    # def self.whos_baby_is_this?(name)
+    #     parent_id = Baby.all.find(baby_id: name)
+    #     parent_id
+    # end
 end
